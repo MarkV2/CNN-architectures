d6061db3a0c47821f3bf70d7209fc8230fc6c73a
MarkV2 <40661992+MarkV2@users.noreply.github.com>
2022-08-30 16:12:27 +0300

    updated file by comparing with official resnet

--- ResNet/ResNetV1_backbone.py
+++ ResNet/ResNetV1_backbone.py
@@ -39,6 +39,7 @@ def ResNetV1(input_shape, output_dims, number_of_identity_blocks):
 
     x = tf.keras.layers.ZeroPadding2D((3, 3))(inp)
     x = conv2D(64, (7, 7), strides=(2, 2))(x)
+    x = tf.keras.layers.ZeroPadding2D((1, 1))(x)
     x = tf.keras.layers.MaxPooling2D((3, 3), strides=(2, 2))(x)
 
     x = convolutional_block([64, 64], (3, 3), strides=(1, 1))(x)
@@ -57,7 +58,7 @@ def ResNetV1(input_shape, output_dims, number_of_identity_blocks):
     for i in range(number_of_identity_blocks[3]):
         x = identity_block([512, 512], (3, 3))(x)
 
-    x = tf.keras.layers.AveragePooling2D((2, 2))(x)
+    x = tf.keras.layers.GlobalAveragePooling2D()(x)
     x = tf.keras.layers.Flatten()(x)
     x = tf.keras.layers.Dense(output_dims)(x)
 
