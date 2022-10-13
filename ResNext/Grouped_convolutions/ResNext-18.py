9e26b151b4a09df23b151902245cd214ba477680
MarkV2 <40661992+MarkV2@users.noreply.github.com>
2022-10-13 15:43:32 +0300

    updated resnext18

--- ResNext/Grouped_convolutions/ResNext-18.py
+++ ResNext/Grouped_convolutions/ResNext-18.py
@@ -1,2 +1,2 @@
-def ResNetXt18(input_shape, output_dims):
-    return ResNetXtV1(input_shape, output_dims, [1, 1, 1, 1])
+def ResNext18(input_shape, output_dims):
+    return ResNextV1(input_shape, output_dims, [1, 1, 1, 1])
