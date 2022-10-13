48b6128942a1a75120123710ee87246b709db271
MarkV2 <40661992+MarkV2@users.noreply.github.com>
2022-10-13 15:41:00 +0300

    Updated resnext50

--- ResNext/Grouped_convolutions/ResNext-50.py
+++ ResNext/Grouped_convolutions/ResNext-50.py
@@ -1,2 +1,2 @@
-def ResNet50(input_shape, output_dims):
-    return ResNetXtV2(input_shape, output_dims, [2, 3, 5, 2])
+def ResNext50(input_shape, output_dims):
+    return ResNextV2(input_shape, output_dims, [2, 3, 5, 2])
